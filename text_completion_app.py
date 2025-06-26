0944fbaa900b20b4d282861f12c0b0d706549e9c
dburnaye <dburnaye@gmu.edu>
2025-06-26 14:57:54 -0400

    Added temperature and top-p displays

--- text_completion_app.py
+++ text_completion_app.py
@@ -2,7 +2,7 @@ import os
 from huggingface_hub import InferenceClient
 
 TOKEN_LIMIT = 5000
-EXIT_KEYWORDS = {"exit", "quit"}
+LEAVE_KEYWORDS = {"exit", "quit"}
 MODELS_DICT = {"sarvamai": "sarvamai/sarvam-m", 
                "llama": "meta-llama/Llama-3.1-8B-Instruct"}
 
@@ -19,11 +19,11 @@ def prompt_user():
     curr_model = ["llama", MODELS_DICT["llama"]]
     settings = [0.5, 0.4]
     while True:
-        response = input(f"You (to {curr_model[0]}): ").strip()
+        response = input(f"You (to {curr_model[0]}); temp: {settings[0]}, top-p: {settings[1]}: ").strip()
         response_split = response.split()
         print("")
 
-        if response.lower() in EXIT_KEYWORDS:
+        if response.lower() in LEAVE_KEYWORDS:
             break
         if response.lower() == "help":
             help_text()
@@ -36,7 +36,7 @@ def prompt_user():
                 option = 0 if response_split[0] == "temperature" else 1
                 val = float(response_split[1])
 
-                if val < 0 or val > 1:
+                if val <= 0 or val >= 1:
                     raise ValueError("The float must be between 0 and 1. Please try again.\n")
                 settings[option] = val
 
@@ -59,7 +59,7 @@ def prompt_user():
 def help_text():
     print("If you want to leave, just type the word \"exit\" or \"quit\" when prompted.")
     print("Type \"sarvamai\" or \"llama\" to access those respective models.")
-    print("Set the creativity and ideation of the AI to a float between 0 and 1 with:\n\t\"temperature float\" and \"top-p float\" respectively.")
+    print("Set the creativity and ideation of all AI models here to floats between 0 and 1 with:\n\t\"temperature float\" and \"top-p float\" respectively.")
     print("If you need these messages again, just type \"help\" when prompted.\n")
 
 def get_ai_response(person_response, selected_model, settings):
@@ -77,7 +77,7 @@ def get_ai_response(person_response, selected_model, settings):
             top_p= settings[1]
         )
 
-        print(f"AI ({selected_model[0]}): ", completion.choices[0].message.content, "\n")
+        print(f"AI ({selected_model[0]}); temp: {settings[0]}, top-p: {settings[1]}: ", completion.choices[0].message.content, "\n")
     except Exception:
         print("The model wasn't able to respond to your prompt in time. Try using the other model or making future prompts shorter. \n")
 
